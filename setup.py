3faef21a6ccfe398e987f555e2a9f212fc3030d7
unknown <mayanktiwarii2002@gmail.com>
2026-02-14 23:07:22 +0530

    Add requirement.txt

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+    name="Price_optimization",
+    version="0.0.1",
+    author="Mayank",
+    packages=find_packages()
+    
+    
+    
+    )
+
+
+
+
+
+
+
