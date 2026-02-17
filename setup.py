1ebc4d4a7af71447e42075a418de3d8b8320b69c
Mayank Tiwari <mayanktiwarii2002@gmail.com>
2026-02-17 06:43:48 +0530

    add files

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 from setuptools import find_packages,setup
+from typing import List
 
+e_dot= "-e ."
+def get_requirements(file_path:str)-> List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements= file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
+        if e_dot in requirements:
+            requirements.remove()
+
+    return requirements
 
 setup(
     name="Price_optimization",
     version="0.0.1",
     author="Mayank",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=get_requirements("requirement.txt")
+)
     
-    
-    
-    )
-
-
-
 
 
 
